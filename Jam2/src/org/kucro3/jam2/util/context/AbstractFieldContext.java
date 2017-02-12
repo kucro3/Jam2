9cb22a1427e7aaf20fcaad27535c7852fe042c60
Kumonda221 <kumonda@foxmail.com>
2017-02-12 20:13:25 +0800

    Preview #17021001

--- Jam2/src/org/kucro3/jam2/util/context/AbstractFieldContext.java
+++ Jam2/src/org/kucro3/jam2/util/context/AbstractFieldContext.java
@@ -1,11 +1,9 @@
 package org.kucro3.jam2.util.context;
 
 import org.kucro3.jam2.util.FieldContext;
-import org.kucro3.util.anno.OverridingRequired;
 
 public abstract class AbstractFieldContext extends AbstractAccessableContext implements FieldContext {
 	@Override
-	@OverridingRequired
 	public abstract String getDeclaringClass();
 	
 	@Override
--- Jam2/src/org/kucro3/jam2/util/context/AbstractMethodContext.java
+++ Jam2/src/org/kucro3/jam2/util/context/AbstractMethodContext.java
@@ -1,11 +1,9 @@
 package org.kucro3.jam2.util.context;
 
 import org.kucro3.jam2.util.MethodContext;
-import org.kucro3.util.anno.OverridingRequired;
 
 public abstract class AbstractMethodContext extends AbstractAccessableContext implements MethodContext {
 	@Override
-	@OverridingRequired
 	public abstract String getDeclaringClass();
 	
 	@Override
