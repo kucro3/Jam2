7435d276d619df6ff32a4bd22dce736819a65c32
Kumonda221 <kumonda@foxmail.com>
2017-12-08 14:24:32 +0800

    Beta #17120403

--- Jam2/src/org/kucro3/jam2/util/builder/structure/BuilderStylizedImplementationView.java
+++ Jam2/src/org/kucro3/jam2/util/builder/structure/BuilderStylizedImplementationView.java
@@ -1,5 +1,11 @@
 package org.kucro3.jam2.util.builder.structure;
 
+import org.kucro3.jam2.util.MethodContext;
+
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
 public class BuilderStylizedImplementationView<T> {
     public BuilderStylizedImplementationView(T upper, ImplementationView view)
     {
@@ -12,7 +18,35 @@ public class BuilderStylizedImplementationView<T> {
         return view;
     }
 
-    // TODO
+    public BuilderStylizedImplementationView<T> foreach(BiConsumer<Integer, ImplementationView.Implementations> consumer,
+                                                        final int travellingDepth)
+    {
+        view.foreach(consumer, travellingDepth);
+        return this;
+    }
+
+    public BuilderStylizedImplementationView<T> foreach(BiConsumer<Integer, ImplementationView.Implementations> consumer)
+    {
+        view.foreach(consumer);
+        return this;
+    }
+
+    public BuilderStylizedImplementationView<T> get(int depth, Consumer<ImplementationView.Implementations> consumer)
+    {
+        consumer.accept(view.get(depth));
+        return this;
+    }
+
+    public BuilderStylizedImplementationView<T> push(Consumer<ImplementationView.Implementations> consumer)
+    {
+        consumer.accept(view.push());
+        return this;
+    }
+
+    public BuilderStylizedImplementations<BuilderStylizedImplementationView<T>> push()
+    {
+        return new BuilderStylizedImplementations<>(this, view.push());
+    }
 
     public T escape()
     {
@@ -22,4 +56,64 @@ public class BuilderStylizedImplementationView<T> {
     final ImplementationView view;
 
     final T upper;
+
+    public static class BuilderStylizedImplementations<T>
+    {
+        public BuilderStylizedImplementations(T upper, ImplementationView.Implementations impls)
+        {
+            this.upper = upper;
+            this.impls = impls;
+        }
+
+        public ImplementationView.Implementations getImplementations()
+        {
+            return impls;
+        }
+
+        public BuilderStylizedImplementations<T> foreach(Consumer<MethodContext.Reflectable> consumer,
+                                                         MethodFilter... filters)
+        {
+            impls.foreach(consumer, filters);
+            return this;
+        }
+
+        public BuilderStylizedImplementations<T> allImplemented(Consumer<Set<Class<?>>> consumer)
+        {
+            consumer.accept(impls.getAllImplemented());
+            return this;
+        }
+
+        public BuilderStylizedImplementations<T> lastImplemented(Consumer<Set<Class<?>>> consumer)
+        {
+            consumer.accept(impls.getLastImplemented());
+            return this;
+        }
+
+        public BuilderStylizedImplementations<T> implementInterfaces(Class<?> type)
+        {
+            impls.implementInterfaces(type);
+            return this;
+        }
+
+        public BuilderStylizedImplementations<T> implement(Class<?> type)
+        {
+            impls.implement(type);
+            return this;
+        }
+
+        public BuilderStylizedImplementations<T> isImplemented(Class<?> type, Consumer<Boolean> consumer)
+        {
+            consumer.accept(impls.isImplemented(type));
+            return this;
+        }
+
+        public T escape()
+        {
+            return upper;
+        }
+
+        final ImplementationView.Implementations impls;
+
+        final T upper;
+    }
 }
